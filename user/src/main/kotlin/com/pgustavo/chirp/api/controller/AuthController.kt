9e8a02e9d439a280db588f3a89182dfc909203f7
Paulo Gustavo <p_gustavo@outlook.com>
2025-12-07 21:55:35 -0300

    Logout System

--- user/src/main/kotlin/com/pgustavo/chirp/api/controller/AuthController.kt
+++ user/src/main/kotlin/com/pgustavo/chirp/api/controller/AuthController.kt
@@ -47,4 +47,11 @@ class AuthController(private val authService: AuthService) {
             .refresh(body.refreshToken)
             .toAuthenticatedUserDto()
     }
+
+    @PostMapping("/logout")
+    fun logout(
+        @RequestBody body: RefreshRequest
+    ){
+      authService.logout(body.refreshToken)
+    }
 }
