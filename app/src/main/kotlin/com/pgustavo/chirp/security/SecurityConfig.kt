564b40081bd33f288d79e2387e38d47d229978f4
Paulo Gustavo <p_gustavo@outlook.com>
2025-12-24 15:54:35 -0300

    Update security config

--- app/src/main/kotlin/com/pgustavo/chirp/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/pgustavo/chirp/security/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package com.pgustavo.chirp.security
 
+import com.pgustavo.chirp.api.config.JwtAuthFilter
+import com.pgustavo.chirp.service.JwtService
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,12 +10,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+    fun filterChain(httpSecurity: HttpSecurity, jwtAuthFilter: JwtAuthFilter): SecurityFilterChain {
         return httpSecurity
             .csrf { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
@@ -21,6 +24,8 @@ class SecurityConfig {
                 auth
                     .requestMatchers("/api/auth/**")
                     .permitAll()
+                    .requestMatchers("/api/auth/change-password")
+                    .authenticated()
                     .dispatcherTypeMatchers(
                         DispatcherType.ERROR,
                         DispatcherType.FORWARD
@@ -29,6 +34,7 @@ class SecurityConfig {
                     .anyRequest()
                     .authenticated()
             }
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling { configurer ->
                 configurer
                     .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
